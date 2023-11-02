9893d6361576e076f96a1c578fe015d375c0e424
setrin <sietrin24@gmail.com>
2023-11-02 10:54:25 +0100

    7th task done

---
+++ 07_grouping_vacations/index.js
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+solve();
+
+function solve() {
+    let data = JSON.parse(fs.readFileSync("./data.json"));
+    let setOfNames = new Set(); //getting all the names
+    for (let record of data) {
+        setOfNames.add(record.user.name);
+    }
+    console.log(setOfNames);
+    let result = organize(data, setOfNames);
+    fs.writeFileSync("./result.json", JSON.stringify(result, null, 2));
+}
+
+function organize(data, setOfNames) {
+    let resultArr = [];
+    for (let name of setOfNames) {
+        resultArr.push({});
+        data.forEach(element => {
+            console.log("heeeey");
+            if (name == element.user.name) {
+                let lastElement = resultArr.length - 1;
+                if (typeof resultArr[lastElement].userId == "undefined") {
+                    resultArr[lastElement].userId = element._id;
+                }
+                if (typeof resultArr[lastElement].name == "undefined") {
+                    resultArr[lastElement].name = name;
+                }
+
+                if (typeof resultArr[lastElement].vacations == "undefined") {
+                    resultArr[lastElement].vacations = [];
+                    resultArr[lastElement].vacations.push({
+                        "startDate": element.startDate,
+                        "endDate": element.endDate,
+                    });
+                }
+                else {
+                    resultArr[lastElement].vacations.push({
+                        "startDate": element.startDate,
+                        "endDate": element.endDate,
+                    });
+                }
+
+            }
+        });
+    }
+    console.log(resultArr);
+    return resultArr;
+}
